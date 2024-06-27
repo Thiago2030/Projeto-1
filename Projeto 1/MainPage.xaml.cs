9dffa51aa8cbcb07ccd851b0e397ce16896db6e3
Thiago <Thiago@DESKTOP-HLR6O3L>
2024-06-27 15:19:51 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto 1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace Projeto_1
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                if (etanol <= (gasolina * 7.0))
+
+                    Title = "O etanol está compensando";
+                else
+
+                    Title = "A gasolina está compensando";
+
+                DisplayAlert("Calculado", Title, "ok");
+
+
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+
+        } // Fecha método 
+    } // Fecha Class 
+
+} // Fecha namespace 
